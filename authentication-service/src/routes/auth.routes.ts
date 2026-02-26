6eda3683181147fd84c4268aa1b23a0b20acf88b
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2026-02-25 21:04:20 -0300

    fix(tcc-12): criar uma tela para esqueci minha senha com validação com jwt, e validação via email

--- authentication-service/src/routes/auth.routes.ts
+++ authentication-service/src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { login, validateToken, verifyTokenHandler } from '../controllers/login.controller';
+import { forgotPassword, validateResetCode, resetPassword } from '../controllers/forgotPassword.controller';
 import { authMiddleware } from '../middleware/authMiddleware';
 import sequelize from '../config/database';
 
@@ -8,6 +9,10 @@ const router = express.Router();
 router.post('/login', login);
 router.post('/validate', validateToken);
 router.get('/verify-token', authMiddleware, verifyTokenHandler);
+
+router.post('/forgot-password', forgotPassword);
+router.post('/validate-code', validateResetCode);
+router.post('/reset-password', resetPassword);
 
 router.get('/health', async (_req, res) => {
   try {
